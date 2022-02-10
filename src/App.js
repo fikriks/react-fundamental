024d1405a5a1e0d0b63139c4043e1f457beaee0f
Fikri Khairul Shaleh <fikriks130@gmail.com>
2022-02-10 21:25:23 +0700

    Map Array :rocket:

--- src/App.js
+++ src/App.js
@@ -50,6 +50,10 @@ function CheckDiscount(props){
 function ProdukInfo(props){
   const { category, name, isDiscount } = props;
   const price = 7400000;
+  const benefits = ["Tidak kusut terkena air", "Bahan lebih halus", "Tidak gerah"];
+  const listBenefits = benefits.map((benefit) =>
+    <li>{benefit}</li>
+  );
 
   return (
     <div>
@@ -59,8 +63,11 @@ function ProdukInfo(props){
           <p className="Price">IDR {price}</p>
           <CheckDiscount isDiscount={isDiscount}/> 
           <p className="Info">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
           </p>
+          <ul>
+            {listBenefits}
+          </ul>
           <a onClick={(e) => TambahCart(name, e)} href="#">Add to Cart</a>
         </div>
     </div>
