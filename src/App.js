7929710d69f8951f5b721a809b5e65a9b643d6b6
Fikri Khairul Shaleh <fikriks130@gmail.com>
2022-02-10 20:57:31 +0700

    Props Component :rocket:

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import './App.css';
 function App() {
   const hello = "Hello World";
   const jsx = <strong>Saya Menggunakan React JSX</strong>;
-  const category = "SNEAKER";
-  const price = 7400000;
   let a = 1;
   let b = 2;
 
@@ -17,15 +15,31 @@ function App() {
       <br/>
         {a + b}
       <br/>
+       <FotoProduk />
+       <ProdukInfo category="SHOES" name="Pinky Swag"/>
+    </div>
+  );
+}
 
-        <div class="Foto">
-          <img src="shoes.jpg" />
-        </div>
-        <div class="Deskripsi">
-          <p class="Cate">{category}</p>
-          <h1 class="Tittle">Pinky Swag</h1>
-          <p class="Price">IDR {price}</p>
-          <p class="Info">
+function FotoProduk() {
+  return (
+      <div className="Foto">
+        <img src="shoes.jpg" />
+      </div>
+  );
+}
+
+function ProdukInfo(props){
+  const { category, name } = props;
+  const price = 7400000;
+
+  return (
+    <div>
+        <div className="Deskripsi">
+          <p className="Cate">{category}</p>
+          <h1 className="Title">{name}</h1>
+          <p className="Price">IDR {price}</p>
+          <p className="Info">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
